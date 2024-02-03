c587994b36a1842789199196a66d5e8778c6fd3b
Wai Min Hein <wai06276@gmail.com>
2024-02-03 23:55:17 +0630

    first

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'gifdb.com',
+            port: '',
+            pathname: '/images/**',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
